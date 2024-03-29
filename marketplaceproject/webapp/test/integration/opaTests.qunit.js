1420910a7cbc54bfc3ee3000685da6b94be29501
Davide Faso <davide.faso@ikonsgroup.it>
2024-03-29 09:33:47 +0000

    Base

---
+++ marketplaceproject/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/sap/marketplaceproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ marketplaceproject/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comsap/marketplaceproject/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
